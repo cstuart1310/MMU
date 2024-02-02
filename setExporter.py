429065b46fbcc93166d02a9380e140283cd3fcc8
cstuart1310 <cstuart1310@gmail.com>
2024-02-02 08:50:37 +0000

    Exports one FBX per child set

--- setExporter.py
+++ setExporter.py
@@ -7,7 +7,7 @@ import os
 
 #print(setList)
 outDir="out/"
-charName="charName_exp" #key used to identify what's relevant
+charName="charName" #key used to identify what's relevant
 
 sceneFullPath= cmds.file(q=True, sn=True) #Gets file name as string
 sceneDir = os.path.dirname(sceneFullPath)
@@ -25,18 +25,14 @@ setList=cmds.listSets( allSets=True, ets=True, o=charName) #all sets in scene in
 
 
 
-# for currentSet in setList: #Can't use the set keyword as its' a thing in python
-    
-#     # print("\nCurrent Set:",currentSet)
-#     # print("Parent:",cmds.listRelatives(currentSet,parent=True))#Shows parent of current set
-    
-#     # print("Children:",cmds.listRelatives(charName,children=True))#Shows array of children of current set
+children = cmds.sets(charName, q=True)
+print(children)
+for childSet in children:
+    cmds.select(childSet)#Selects the current set
+    outName=sceneDir+"/"+sceneNo+"_"+shotNo+"_"+charName+"_"+childSet+".fbx" #scene_shot_character_joint 
+    print("Out path:",outName)
+    #cmds.FBXExport('-file', outName)
 
-cmds.select(charName)#Selects the current set
-outName=sceneDir+"/"+shotNo+"_"+charName+"_"+charName+".fbx" #scene_shot_character_joint 
-print("Out path:",outName)
-#cmds.FBXExport('-file', outName)
-
-fbxCommand='file -force -options "" -typ "FBX export" -pr -es "'+outName+'";'
-print(fbxCommand)
-mel.eval(fbxCommand)
+    fbxCommand='file -force -options "" -typ "FBX export" -pr -es "'+outName+'";'
+    print(fbxCommand)
+    mel.eval(fbxCommand)
