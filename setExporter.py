2bb2e516a3583311034ae17e0c891439a0db20a7
cstuart1310 <cstuart1310@gmail.com>
2024-02-02 07:17:27 +0000

    Filters to produce correctly named output path

--- setExporter.py
+++ setExporter.py
@@ -4,7 +4,7 @@ import maya.cmds as cmds
 
 #print(setList)
 outDir="out/"
-exportKey="charName" #key used to identify what's relevant
+charName="charName" #key used to identify what's relevant
 
 sceneName= cmds.file(q=True, sn=True) #Gets file name as string
 sceneNo=(sceneName.split("_")[0]).split("/")[1]
@@ -15,13 +15,7 @@ print("Scene No:",sceneNo)
 print("Shot No",shotNo)
 print("versionNo",versionNo)
 
-exportKey="charName"
-sceneName= cmds.file(q=True, sn=True)
 print("Exporting:")
-print(setList)
-for mayaSet in setList: #Can't use the set keyword as its' a thing in python
-    if exportKey in mayaSet:#If the set is set to be exported
-        print(mayaSet)
         cmds.select(mayaSet)#Selects the current set
         outName="out/"+sceneName+"_"+mayaSet+".fbx"
         #cmds.FBXExport('-file', outName)
