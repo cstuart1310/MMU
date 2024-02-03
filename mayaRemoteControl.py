dfdd51d6e0a358a5f77fbcf96cc1907777c1a0a3
cstuart1310 <cstuart1310@gmail.com>
2024-02-03 13:34:16 +0000

    Lists sets from scene headlessly
    Needs to run in MayaPy from bin folder

---
+++ mayaRemoteControl.py
@@ -0,0 +1,39 @@
+import maya.standalone
+import maya.cmds as cmds
+
+maya.standalone.initialize(name='python')#Inits a headless maya instance
+
+
+
+def printSets(sceneFile):
+    # Open the Maya scene file
+    cmds.file(sceneFile, open=True, force=True)
+
+    # Get all items from the outliner
+    outliner_items = cmds.ls(assemblies=True, long=True)
+    mayaSets=cmds.ls(type='objectSet')
+    # Print the outliner items
+    print("Sets:")
+    for mayaSet in mayaSets:
+        print(mayaSet)
+
+    # Quit Maya standalone mode
+    maya.standalone.uninitialize()
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 2:
+        print("Usage: python maya_batch_script.py <sceneFile>")
+        sys.exit(1)
+
+    sceneFile = sys.argv[1]
+
+    # Redirect Maya startup log to a file
+    log_file = "maya_batch_log.txt"
+    maya.standalone.initialize(name='python', standalone=True, plugin=True, script=True, verbose=False, outputLog=log_file)
+
+    printSets(sceneFile)
+
+    # Close the log file
+    maya.standalone.uninitialize()
