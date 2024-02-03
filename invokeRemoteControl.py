234c9801b3ba82904b8ae0cb1264c04d2ec57105
cstuart1310 <cstuart1310@gmail.com>
2024-02-03 15:13:53 +0000

    Exports sets to FBX headlessly
    Opens a headless maya instance and outputs the set "set1" as an FBX

--- invokeRemoteControl.py
+++ invokeRemoteControl.py
@@ -5,6 +5,6 @@ mayaBinDir=r"C:\Program Files\Autodesk\Maya2024\bin"#Dir containing mayapy
 
 pwd=os.getcwd()#Dir containing MMU scripts so they can be called
 scenePath=r"C:\Users\Callum\Documents\GitHub\MMU\sc004_sh001_v1.mb" #scene to be opened
-mayaCommand=mayaBinDir+"\mayapy mayaRemoteControl.py "+scenePath #Command to be run
+mayaCommand=mayaBinDir+"\mayapy setExporter.py "+scenePath #Command to be run
 
 subprocess.Popen(mayaCommand)#runs the command
--- setExporter.py
+++ setExporter.py
@@ -1,48 +1,80 @@
 #Callum Stuart - https://linkedin.callumstuart.com https://github.com/cstuart1310 https://showreel.callumstuart.com
-#Takes a selected set, then exports it and its' children as individual FBX files
 
 print("\n"*30)#Spacing because I cant be bothered to keep clearing the log
 import maya.cmds as cmds
 import maya.mel as mel #evaluating FBX exporter because afaik it doesn't have cmds integration
+import maya.standalone #runs maya headlessly
 import os
+import sys #arg reading
 
-# Get a list of all the deformer sets in the scene:
 
 
-parent=cmds.ls(sl=True,long=True)[0] #gets currently selected item
+def initMaya():
+    maya.standalone.initialize(name='python')
+    cmds.file(sceneFile, open=True, force=True)
 
-exportAsIndividual=False #Whether or not to export each child as a single FBX each, or export all children as one FBX
+def getSets():
+    # Get all sets from the outliner
+    mayaSets=cmds.ls(type='objectSet')
+    return mayaSets
 
-#Based on assumption of sceneNo_shotNo_versionNo
-sceneFullPath= cmds.file(q=True, sn=True) #Gets file name as string
-sceneDir = os.path.dirname(sceneFullPath)
-sceneNo=(sceneFullPath.split("_")[0]).split("/")[-1]
-shotNo=sceneFullPath.split("_")[1]
-versionNo=(sceneFullPath.split("_")[2]).replace(".mb","")
 
-print("Scene dir:",sceneDir)
-print("Scene No:",sceneNo)
-print("Shot No:",shotNo)
-print("versionNo:",versionNo)
+def getOutName(exportAsIndividual,parent,child):#generates out path for FBX
+    #Based on assumption of sceneNo_shotNo_versionNo
+    sceneFullPath= cmds.file(q=True, sn=True) #Gets file name as string
+    sceneDir = os.path.dirname(sceneFullPath)
+    sceneNo=(sceneFullPath.split("_")[0]).split("/")[-1]
+    shotNo=sceneFullPath.split("_")[1]
+    versionNo=(sceneFullPath.split("_")[2]).replace(".mb","")
 
-print("Exporting:")
-setList=cmds.listSets(allSets=True, ets=True, o=parent) #all sets in scene including irrelevant stuff
+    print("Scene dir:",sceneDir)
+    print("Scene No:",sceneNo)
+    print("Shot No:",shotNo)
+    print("versionNo:",versionNo)
 
-children = cmds.sets(parent, q=True)
-print("Children:",children)
-if children==None:#If children found is type None (Not to be confused with finding no children)
-    cmds.error("No children found. Check that a set is selected and that it has children.")
-else:#If children are found
-    for child in children:#Loops through each child in the set (Grandchildren are selected when their parent is)
-        
-        if exportAsIndividual==True:#One FBX per child (Grandchildren merged with parents)
-            cmds.select(child,add=False)#Adds the child to the selection
-            outName=sceneDir+"/"+sceneNo+"_"+shotNo+"_"+parent+"_"+child+".fbx" #scene_shot_character_joint
-        elif exportAsIndividual==False:#One FBX per set (Set's children, grandchildren etc all in one FBX)
-            cmds.select(child,add=True)
-            outName=sceneDir+"/"+sceneNo+"_"+shotNo+"_"+parent+".fbx" #scene_shot_character_joint
+    if exportAsIndividual==True:
+        outName=sceneDir+"/"+sceneNo+"_"+shotNo+"_"+parent+"_"+child+".fbx" #scene_shot_character_joint
+    elif exportAsIndividual==False:
+        outName=sceneDir+"/"+sceneNo+"_"+shotNo+"_"+parent+".fbx" #scene_shot_character_joint
 
-        
-        print("Out path:",outName)
-        fbxCommand='file -force -options "" -typ "FBX export" -pr -es "'+outName+'";'#exports using default FBX export settings
-        mel.eval(fbxCommand)#Runs the command within MEL as there is no CMDS integration
+    return outName
+
+def chooseSet(mayaSets):
+        # Print the sets
+    print("Sets:")
+    for setIndex, mayaSet in enumerate(mayaSets):
+        print(setIndex,mayaSet)
+    chosenSetIndex=input("Set to export:")
+    return mayaSets[chosenSetIndex]
+
+
+def exportSet(parent):
+    children = cmds.sets(parent, q=True)#All children of set
+    print("Children:",children)
+    if children==None:#If children found is type None (Not to be confused with finding no children)
+        cmds.error("No children found. Check that a set is selected and that it has children.")
+    else:#If children are found
+        for child in children:#Loops through each child in the set (Grandchildren are selected when their parent is)
+            if exportAsIndividual==True:#One FBX per child (Grandchildren merged with parents)
+                cmds.select(child,add=False)#Adds the child to the selection
+            elif exportAsIndividual==False:#One FBX per set (Set's children, grandchildren etc all in one FBX)
+                cmds.select(child,add=True)#Replaces the selection with the children
+            outName=getOutName(exportAsIndividual,parent,child)#Generates path for FBX to write to
+            print("Out path:",outName)
+            fbxCommand='file -force -options "" -typ "FBX export" -pr -es "'+outName+'";'#exports using default FBX export settings
+            print("Output command:",fbxCommand)
+            cmds.loadPlugin("fbxmaya.mll")#Ensures the FBX plugin is loaded (May not due to maya standalone)
+            mel.eval(fbxCommand)#Runs the command within MEL as there is no CMDS integration
+
+
+#main
+exportAsIndividual=True #Whether or not to export each child as a single FBX each, or export all children as one FBX
+sceneFile = sys.argv[1] #Gets scene path from argument
+initMaya()
+mayaSets=getSets()
+chosenSet="set1"#chooseSet(mayaSets)
+exportSet(chosenSet)
+
+
+# Close the instance to prevent memory leak
+maya.standalone.uninitialize()
