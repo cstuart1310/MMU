c5ce6360bf5ed46ad0b749e253d0a3717c479fc7
cstuart1310 <cstuart1310@gmail.com>
2024-02-03 08:59:31 +0000

    Now operates on the currently selected set

--- setExporter.py
+++ setExporter.py
@@ -1,3 +1,6 @@
+#Callum Stuart - https://linkedin.callumstuart.com https://github.com/cstuart1310 https://showreel.callumstuart.com
+#Takes a selected set, then exports it and its' children as individual FBX files
+
 print("\n"*30)#Spacing because I cant be bothered to keep clearing the log
 import maya.cmds as cmds
 import maya.mel as mel #evaluating FBX exporter because afaik it doesn't have cmds integration
@@ -5,9 +8,8 @@ import os
 
 # Get a list of all the deformer sets in the scene:
 
-#print(setList)
-outDir="out/"
-charName="charName" #key used to identify what's relevant
+
+parentSet=cmds.ls(sl=True,long=True)[0] #gets currently selected set
 
 sceneFullPath= cmds.file(q=True, sn=True) #Gets file name as string
 sceneDir = os.path.dirname(sceneFullPath)
@@ -21,17 +23,14 @@ print("Shot No:",shotNo)
 print("versionNo:",versionNo)
 
 print("Exporting:")
-setList=cmds.listSets( allSets=True, ets=True, o=charName) #all sets in scene including irrelevant stuff
+setList=cmds.listSets( allSets=True, ets=True, o=parentSet) #all sets in scene including irrelevant stuff
 
-
-
-children = cmds.sets(charName, q=True)
+children = cmds.sets(parentSet, q=True)
 print(children)
 for childSet in children:
     cmds.select(childSet)#Selects the current set
-    outName=sceneDir+"/"+sceneNo+"_"+shotNo+"_"+charName+"_"+childSet+".fbx" #scene_shot_character_joint 
+    outName=sceneDir+"/"+sceneNo+"_"+shotNo+"_"+parentSet+"_"+childSet+".fbx" #scene_shot_character_joint
     print("Out path:",outName)
-    #cmds.FBXExport('-file', outName)
 
     fbxCommand='file -force -options "" -typ "FBX export" -pr -es "'+outName+'";'
     print(fbxCommand)
