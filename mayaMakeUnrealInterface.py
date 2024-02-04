945f21668b23f07ab2f57b54e2708b82035c7d4c
cstuart1310 <cstuart1310@gmail.com>
2024-02-04 16:38:28 +0000

    Clean restarting of UI instances

--- mayaMakeUnrealInterface.py
+++ mayaMakeUnrealInterface.py
@@ -4,12 +4,12 @@ import subprocess #Call the setExporter from within the mayaPy interpreter
 import os #File removing
 import sys #Reading command-line arguments
 import unreal
-from PySide2.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+from PySide2.QtCore import QSize, Qt
+from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
 
-
-
+#-----File import/exporting-----
 def beginSetExporter(mayaBinDir,pluginDir,scenePath):
     mayaCommand=mayaBinDir+"\mayapy "+pluginDir+"\setExporter.py "+scenePath #Command to be run
     print(mayaCommand)
@@ -41,7 +41,36 @@ def importFilebox(importPath):
     unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks([import_task])    #Execute the import task
     os.remove(importPath) #Deletes the FBX produced by the exporter, as it is now saved within the UE project.
 
-#main
+
+#-----UI-----
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        button = QPushButton("Press Me!")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
+    def the_button_was_clicked(self):
+        print("Clicked!")
+
+def initUI():
+    if not QApplication.instance():#If an instance does not exist
+        app = QApplication(sys.argv)#Launch a new Qapplication
+    else:#If an instance does exist
+        app = QApplication.instance()#Launch a new instance
+
+    window = MainWindow()
+    window.show()
+
+    app.exec_()
+
+#-----main-----
 # mayaBinDir=sys.argv[1]#Dir containing mayapy interpreter
 # pluginDir=sys.argv[2] #Path containing these scripts/shared data
 # scenePath=sys.argv[3] #scene to be opened
@@ -49,35 +78,4 @@ def importFilebox(importPath):
 mayaBinDir=r"C:\Program Files\Autodesk\Maya2023\bin"#Dir containing mayapy
 pluginDir=r"D:\Coding\Python\VFX\Maya\MayaToUnreal\mayaToUnreal"
 scenePath=r"C:\Users\CallyWally\Documents\CS-MR-2TB\Modelling\MMU\scenes\sceneNo_shotNo_versionNo.mb" #scene to be opened
-
-# Create a Qt application
-app = QApplication([])
-
-# Create a Qt widget
-widget = QWidget()
-widget.setWindowTitle("Unreal Engine Qt UI")
-widget.setGeometry(100, 100, 300, 200)
-
-# Create a layout for the widget
-layout = QVBoxLayout(widget)
-
-# Create a button
-button = QPushButton("Click me!")
-layout.addWidget(button)
-
-# Define a function to be called when the button is clicked
-def on_button_click():
-    unreal.log("Button clicked!")
-    beginSetExporter(mayaBinDir,pluginDir,scenePath)
-    
-
-# Connect the button click event to the function
-button.clicked.connect(on_button_click)
-
-# Show the widget
-widget.show()
-
-# Start the Qt application event loop
-app.exec_()
-
 
