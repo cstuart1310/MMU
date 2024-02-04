63f26752d1ba4e50965491ec172fb632ec16ac16
cstuart1310 <cstuart1310@gmail.com>
2024-02-04 17:06:08 +0000

    Reads UI from file in plugin root

--- mayaMakeUnrealInterface.py
+++ mayaMakeUnrealInterface.py
@@ -4,8 +4,11 @@ import subprocess #Call the setExporter from within the mayaPy interpreter
 import os #File removing
 import sys #Reading command-line arguments
 import unreal
+from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtUiTools import QUiLoader
 from PySide2.QtCore import QSize, Qt
 from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton
+
 
 
 
@@ -43,31 +46,16 @@ def importFilebox(importPath):
 
 
 #-----UI-----
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("My App")
-
-        button = QPushButton("Press Me!")
-        button.setCheckable(True)
-        button.clicked.connect(self.the_button_was_clicked)
-
-        # Set the central widget of the Window.
-        self.setCentralWidget(button)
-
-    def the_button_was_clicked(self):
-        print("Clicked!")
 
 def initUI():
+    loader = QUiLoader()
     if not QApplication.instance():#If an instance does not exist
         app = QApplication(sys.argv)#Launch a new Qapplication
     else:#If an instance does exist
         app = QApplication.instance()#Launch a new instance
-
-    window = MainWindow()
+    uiPath=pluginDir+"\MMU_UE_UI.ui"
+    window = loader.load(uiPath, None)
     window.show()
-
     app.exec_()
 
 #-----main-----
@@ -76,6 +64,8 @@ def initUI():
 # scenePath=sys.argv[3] #scene to be opened
 
 mayaBinDir=r"C:\Program Files\Autodesk\Maya2023\bin"#Dir containing mayapy
-pluginDir=r"D:\Coding\Python\VFX\Maya\MayaToUnreal\mayaToUnreal"
+pluginDir=(os.path.realpath(__file__)).replace("mayaMakeUnrealInterface.py","")
 scenePath=r"C:\Users\CallyWally\Documents\CS-MR-2TB\Modelling\MMU\scenes\sceneNo_shotNo_versionNo.mb" #scene to be opened
 
+
+initUI()
