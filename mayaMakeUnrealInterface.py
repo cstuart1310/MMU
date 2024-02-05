fa602f9fbe95a629fda21d0a80d36c0e3c764af3
cstuart1310 <cstuart1310@gmail.com>
2024-02-05 10:25:57 +0000

    Linked UI to functions

--- mayaMakeUnrealInterface.py
+++ mayaMakeUnrealInterface.py
@@ -47,16 +47,33 @@ def importFilebox(importPath):
 
 #-----UI-----
 
-def initUI():
+def initUI():#Launches the UI
     loader = QUiLoader()
     if not QApplication.instance():#If an instance does not exist
         app = QApplication(sys.argv)#Launch a new Qapplication
     else:#If an instance does exist
         app = QApplication.instance()#Launch a new instance
     uiPath=pluginDir+"\MMU_UE_UI.ui"
-    window = loader.load(uiPath, None)
+    window = loader.load(uiPath, None)#Loads UI from file
+    window.lineEdit_pluginPath.setText(pluginDir)#Sets the text in the plugin path to the gotten value
+    window.pushButton.clicked.connect(lambda: getInputData(window))
     window.show()
     app.exec_()
+
+def getInputData(window):#Gets data from input boxes and then passes it to the maya parts of the script
+    print("Getting inputted data")
+    mayaBinDir=window.lineEdit_binPath.text()
+    pluginDir=window.lineEdit_pluginPath.text()
+    scenePath=window.lineEdit_scenePath.text()
+    scenePath=r"C:\Users\CallyWally\Documents\CS-MR-2TB\Modelling\MMU\scenes\sceneNo_shotNo_versionNo.mb" #scene to be opened
+    mayaBinDir=r"C:\Program Files\Autodesk\Maya2023\bin"#Dir containing mayapy
+    pluginDir=r"D:\Coding\Python\VFX\Maya\MayaToUnreal\mayaToUnreal"
+
+    print(mayaBinDir)
+    print(pluginDir)
+    print(scenePath)
+    beginSetExporter(mayaBinDir,pluginDir,scenePath)
+    window.close()
 
 #-----main-----
 # mayaBinDir=sys.argv[1]#Dir containing mayapy interpreter
@@ -65,7 +82,9 @@ def initUI():
 
 mayaBinDir=r"C:\Program Files\Autodesk\Maya2023\bin"#Dir containing mayapy
 pluginDir=(os.path.realpath(__file__)).replace("mayaMakeUnrealInterface.py","")
+print(pluginDir)
 scenePath=r"C:\Users\CallyWally\Documents\CS-MR-2TB\Modelling\MMU\scenes\sceneNo_shotNo_versionNo.mb" #scene to be opened
 
 
 initUI()
+
